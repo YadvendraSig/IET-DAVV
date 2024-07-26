6c50535ed500d6682d07e4baf4ab1a2a5bf0a958
YadvendraSig <yadvedrasinghchouhan@gmail.com>
2024-07-26 22:34:12 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,27 @@
+public class Data {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = true;
+        short c = -200;
+        byte d = 124;
+        long e = -423322000L;
+        double f = -42.3;
+        float g = -42.7f;
+        char h ='D';
+
+        System.out.println( a);
+        System.out.println( b);
+       System.out.println( c);
+       System.out.println( d);
+        System.out.println( e);
+       System.out.println( f);
+       System.out.println( g);
+        System.out.println( h);
+       
+
+
+
+
+    }
+    
+}
